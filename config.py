5c56d1d709d99d74c49e2c9035b0f0ddec672a5b
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2021-01-09 13:20:44 -0600

    postgres link

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connects to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:lawpal86@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://mcdczwsgmvrrdv:c0d960b1817b35a3b30e545a7b06f81df2dd193b24ab47b1c8db8f97b4871071@ec2-52-205-99-67.compute-1.amazonaws.com:5432/der1ug5f2c5p08'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
